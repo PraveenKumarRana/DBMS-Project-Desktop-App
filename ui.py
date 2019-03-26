8b976773fc5702ebc499046677d2d4af2a830ff1
Saumya Prakash <saumyaprakash30@gmail.com>
2019-03-27 00:08:41 +0530

    ui

--- ui.py
+++ ui.py
@@ -11,7 +11,22 @@ class OtherFrame(wx.Frame):
     def __init__(self, title, parent=None):
         wx.Frame.__init__(self, parent=parent, title=title)
         self.SetSize((800, 600))
+        self.Centre()
+        pnl=ebPanel(self)
+
+
+
+
         self.Show()
+class ebPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent,pos,size)
+        self.SetBackgroundColour("blue")
+        self.Centre(self)
+        #eb_back = wx.Button(self, label='Back')
+        #eb_back.Bind(wx.EVT_BUTTON, self.OnClose1)
+    def OnClose1(self,e):
+        self.Close(True)
 
 class Example(wx.Frame):
 
@@ -24,9 +39,9 @@ class Example(wx.Frame):
 
         pnl = wx.Panel(self)
         pcButton = wx.Button(pnl, label='Power Company', pos=(40, 30),size=(200,40))
-	dcButton = wx.Button(pnl, label='Distribution Company', pos=(40, 80),size=(200,40))
-	tcButton = wx.Button(pnl, label='Transmission Company', pos=(40, 130),size=(200,40))
-	ebButton = wx.Button(pnl, label='Electricity Board', pos=(40, 180),size=(200,40))
+        dcButton = wx.Button(pnl, label='Distribution Company', pos=(40, 80),size=(200,40))
+        tcButton = wx.Button(pnl, label='Transmission Company', pos=(40, 130),size=(200,40))
+        ebButton = wx.Button(pnl, label='Electricity Board', pos=(40, 180),size=(200,40))
         pcButton.Bind(wx.EVT_BUTTON, self.OnClose)
         dcButton.Bind(wx.EVT_BUTTON, self.OnClose)
         tcButton.Bind(wx.EVT_BUTTON, self.OnClose)
@@ -46,8 +61,14 @@ class Example(wx.Frame):
 
     def eb(self,e):
     	title = 'ELECTRICITY BOARD'
-        frame = OtherFrame(title=title)
-        self.Hide()
+        #frame = OtherFrame(title=title)
+        pnl=ebPanel(self,size=(100,100))
+
+
+
+
+
+
 
 
     def OnClose(self,e):
