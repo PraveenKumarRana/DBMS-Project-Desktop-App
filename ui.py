05c2be04c031f93d60b04c3de8a84a4b4c10b2ce
TheVaibhavSinghal <singhal.vaibhav2303@gmail.com>
2019-03-28 19:15:12 +0530

    Added User profile page

--- ui.py
+++ ui.py
@@ -5,11 +5,11 @@ import wx
 w=0
 h=0
 class NewPanel(wx.Panel):
-    
+
     def __init__(self, parent):
         """Constructor"""
         wx.Panel.__init__(self, parent=parent,size=(w,h),pos=(0,0))
-        
+
 
 class MainWindow(wx.Frame):
 
@@ -17,12 +17,12 @@ class MainWindow(wx.Frame):
         wx.Frame.__init__(self, parent=None,style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
         self.SetIcon(wx.Icon("display1.ico"))
         self.InitUI()
-        
+
 
     def InitUI(self):
 
         global w,h
-        
+
         menubar = wx.MenuBar()
         fileMenu = wx.Menu()
         fileItem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
@@ -30,7 +30,7 @@ class MainWindow(wx.Frame):
         self.SetMenuBar(menubar)
 
         self.Bind(wx.EVT_MENU, self.OnClose, fileItem)
-        
+
         self.homepnl = wx.Panel(self)
         pcButton = wx.Button(self.homepnl, label='Power Company', pos=(40, 30),size=(200,40))
         dcButton = wx.Button(self.homepnl, label='Distribution Company', pos=(40, 80),size=(200,40))
@@ -40,13 +40,13 @@ class MainWindow(wx.Frame):
         dcButton.Bind(wx.EVT_BUTTON, self.dc)
         tcButton.Bind(wx.EVT_BUTTON, self.tc)
         ebButton.Bind(wx.EVT_BUTTON, self.eb)
-        
-        l1 = wx.StaticText(self.homepnl, -1, "CustomerID",pos=(310,40)) 
+
+        l1 = wx.StaticText(self.homepnl, -1, "CustomerID",pos=(310,40))
         self.t1 = wx.TextCtrl(self.homepnl,pos=(400,30),size=(200,40))
         l1 = wx.StaticText(self.homepnl, -1, "Password",pos=(310,90))
         self.t2 = wx.TextCtrl(self.homepnl,style = wx.TE_PASSWORD,pos=(400,80),size=(200,40))
         loginButton = wx.Button(self.homepnl, label='Login', pos=(515, 130))
-        loginButton.Bind(wx.EVT_BUTTON, self.OnClose)
+        loginButton.Bind(wx.EVT_BUTTON, self.UserProfile)   #change to customer later
         w,h=wx.GetDisplaySize()
         self.SetSize((w,h))
         self.SetTitle('Power Distribution System')
@@ -62,12 +62,12 @@ class MainWindow(wx.Frame):
         self.p2=self.homepnl
     	ebButton.Bind(wx.EVT_BUTTON, self.back)
         self.ebpnl.Show()
-        
+
     def back(self,e):
     	self.p1.Hide()
     	self.p2.Show()
     	self.SetTitle(self.previousTitle)
-    	
+
     def pc(self,e):
         self.homepnl.Hide()
         self.previousTitle=self.GetTitle()
@@ -101,10 +101,21 @@ class MainWindow(wx.Frame):
     	BackButton.Bind(wx.EVT_BUTTON,self.back)
     	self.tcpnl.Show()
 
-        
-        
-        
-    	
+    def UserProfile(self,e):
+    	self.homepnl.Hide()  #change to custpnl later
+        self.previousTitle=self.GetTitle()
+    	self.SetTitle("User Profile")
+        self.uppnl=NewPanel(self)
+        l1=wx.StaticText(self.uppnl, -1, "Password",pos=(310,90),size=(1000,1000))
+        l1.SetFont(wx.Font(18, wx.MODERN, wx.NORMAL, wx.BOLD))
+        BackButton = wx.Button(self.uppnl, label='Back', pos=(60, 420),size=(100,40))
+    	self.p1=self.uppnl
+    	self.p2=self.homepnl
+    	BackButton.Bind(wx.EVT_BUTTON,self.back)
+    	#self.uppnl.SetBackgroundColour("blue")
+    	self.uppnl.Show()
+
+
     def OnClose(self,e):
 
         self.Close(True)
@@ -119,5 +130,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()  
-
+    main()
