94d2d7628592b5690fe5a7616852c87dae13214a
Saumya Prakash <saumyaprakash30@gmail.com>
2019-03-29 17:00:50 +0530

    added display table in ele board

--- ui.py
+++ ui.py
@@ -2,14 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import wx
+import MySQLdb as mdb
+con = mdb.connect('localhost', 'admin', 'admin', 'eds')
 w=0
 h=0
 class NewPanel(wx.Panel):
-    
+
     def __init__(self, parent):
         """Constructor"""
         wx.Panel.__init__(self, parent=parent,size=(w,h),pos=(0,0))
-        
+
 
 class MainWindow(wx.Frame):
 
@@ -17,12 +19,12 @@ class MainWindow(wx.Frame):
         wx.Frame.__init__(self, parent=None,style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
         self.SetIcon(wx.Icon("display1.ico"))
         self.InitUI()
-        
+
 
     def InitUI(self):
 
         global w,h
-        
+
         menubar = wx.MenuBar()
         fileMenu = wx.Menu()
         fileItem = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
@@ -30,7 +32,7 @@ class MainWindow(wx.Frame):
         self.SetMenuBar(menubar)
 
         self.Bind(wx.EVT_MENU, self.OnClose, fileItem)
-        
+
         self.homepnl = wx.Panel(self)
         pcButton = wx.Button(self.homepnl, label='Power Company', pos=(40, 30),size=(200,40))
         dcButton = wx.Button(self.homepnl, label='Distribution Company', pos=(40, 80),size=(200,40))
@@ -40,8 +42,8 @@ class MainWindow(wx.Frame):
         dcButton.Bind(wx.EVT_BUTTON, self.dc)
         tcButton.Bind(wx.EVT_BUTTON, self.tc)
         ebButton.Bind(wx.EVT_BUTTON, self.eb)
-        
-        l1 = wx.StaticText(self.homepnl, -1, "CustomerID",pos=(310,40)) 
+
+        l1 = wx.StaticText(self.homepnl, -1, "CustomerID",pos=(310,40))
         self.t1 = wx.TextCtrl(self.homepnl,pos=(400,30),size=(200,40))
         l1 = wx.StaticText(self.homepnl, -1, "Password",pos=(310,90))
         self.t2 = wx.TextCtrl(self.homepnl,style = wx.TE_PASSWORD,pos=(400,80),size=(200,40))
@@ -57,17 +59,51 @@ class MainWindow(wx.Frame):
     	self.previousTitle=self.GetTitle()
     	self.SetTitle("Electricity Board")
         self.ebpnl= NewPanel(self)
-        ebButton = wx.Button(self.ebpnl, label='Back', pos=(10, 10))
+        ebButton = wx.Button(self.ebpnl, label='Back', pos=(1000, 10))
         self.p1=self.ebpnl
         self.p2=self.homepnl
     	ebButton.Bind(wx.EVT_BUTTON, self.back)
+        #l1 = wx.StaticText(self.ebpnl, -1,"hello",pos=(10,10))
+        with con:
+            cur = con.cursor(mdb.cursors.DictCursor)
+            cur.execute("SELECT * FROM electricityboard limit 4")
+            rows = cur.fetchall()
+            desc = cur.description
+
+        wx.StaticText(self.ebpnl, -1,"Board Name",pos=(70,50))
+        wx.StaticText(self.ebpnl, -1,"No. of Consumer",pos=(270,50))
+        wx.StaticText(self.ebpnl, -1,"State",pos=(470,50))
+        wx.StaticText(self.ebpnl, -1,"Power Consumed",pos=(600,50))
+        dc = wx.ClientDC(self)
+        dc.DrawLine(50, 60, 190, 60)
+
+        #print "%s %s %s %s %s" % (desc[0][0], desc[1][0],desc[2][0],desc[3][0],desc[4][0])
+        i=100
+        for row in rows:
+            #txt = row[desc[0][0]], row[desc[1][0]], row[desc[2][0]],row[desc[3][0]],row[desc[4][0]]
+            wx.StaticText(self.ebpnl, -1,row[desc[0][0]],pos=(80,i))
+            wx.StaticText(self.ebpnl, -1,str(row[desc[1][0]]),pos=(270,i))
+            wx.StaticText(self.ebpnl, -1,row[desc[2][0]],pos=(470,i))
+            wx.StaticText(self.ebpnl, -1,str(row[desc[4][0]]),pos=(600,i))
+
+            i=i+30
+
+
+
+
+
+
+
+
+
+
         self.ebpnl.Show()
-        
+
     def back(self,e):
     	self.p1.Hide()
     	self.p2.Show()
     	self.SetTitle(self.previousTitle)
-    	
+
     def pc(self,e):
         self.homepnl.Hide()
         self.previousTitle=self.GetTitle()
@@ -78,6 +114,7 @@ class MainWindow(wx.Frame):
     	self.p2=self.homepnl
     	BackButton.Bind(wx.EVT_BUTTON,self.back)
     	self.pcpnl.Show()
+
 
     def dc(self,e):
         self.homepnl.Hide()
@@ -101,10 +138,10 @@ class MainWindow(wx.Frame):
     	BackButton.Bind(wx.EVT_BUTTON,self.back)
     	self.tcpnl.Show()
 
-        
-        
-        
-    	
+
+
+
+
     def OnClose(self,e):
 
         self.Close(True)
@@ -119,5 +156,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()  
-
+    main()
