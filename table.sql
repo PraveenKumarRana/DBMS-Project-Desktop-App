d84850ee7e33198af232f6fcb0b162f3688d83d8
Firoz1999 <firozmohd673@gmail.com>
2019-03-29 13:19:32 -0400

    phone email added

--- table.sql
+++ table.sql
@@ -19,9 +19,9 @@ insert into employee values
 	(7001,	'Amar kumar',		'2012-08-11',	'2000-11-06',	'dept',	18,	'boardname',	'designation',	844667,	'password'),
 	(7002,	'firoz mohammad',	'2010-10-08',	'1999-12-06',	'dept',	19,	'boardname',	'designation',	129748,	'password'),
 	(7003,	'saumya prakash',	'2013-12-03',	'1998-06-10',	'dept',	21,	'boardname',	'designation',	729733,	'password'),
-	(7004,	'vaibhav singhal',	'2015-19-07',	'1999-03-23',	'dept',	20,	'boardname',	'designation',	264856,	'password'),
+	(7004,	'vaibhav singhal',	'2015-09-07',	'1999-03-23',	'dept',	20,	'boardname',	'designation',	264856,	'password'),
 	(7005,	'Praveen Rana',		'2012-03-05',	'1998-12-12',	'dept',	20,	'boardname',	'designation',	197835,	'password');
-	
+
 insert into consumer values
 	(8001,	'John',		254789,	'boardname',	'Delhi',			'subdiv',	'divis',	'delhi-NCR',	101,	'password',	'john@gmail.com',		'GT road noida'		),
 	(8002,	'Andrew',	961315,	'boardname',	'Assam',			'subdiv',	'divis',	'dispur',	102,	'password',	'Andrew@gmail.com',		'Ak sahaylane dispur'	),
@@ -66,7 +66,7 @@ insert into subdivision values
 	(6005,	5005,	'Anurag',	'Gujarat',			'sdivname');
 insert into electricityboard values
 	('ASEC',	250000,	'Assam',		301,	2500),
-	('DVB',		400000,	'Delhi',		302,	3200),	
+	('DVB',		400000,	'Delhi',		302,	3200),
 	('GUVN',	610000,	'Gujarat',		303,	4000),
 	('HPGC',	120000,	'Haryana',		304,	1600),
 	('JSEB',	320000,	'Jharkhand',		305,	5600);
